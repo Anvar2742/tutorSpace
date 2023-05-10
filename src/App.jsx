0eee26e6af07ab183f0dca47fdd15608bc11c93d
marco12-star <99267663+Anvar2742@users.noreply.github.com>
2023-05-10 20:15:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <header>
+                <a href="#" className="text-black">
+                    tutorSpace
+                </a>
+                <a href="#">Log in</a>
+            </header>
+            <h1 className="text-3xl font-bold">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;

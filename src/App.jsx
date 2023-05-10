8751b8ee7046c7fcc12ee09b137e9cb394b8cd47
marco12-star <99267663+Anvar2742@users.noreply.github.com>
2023-05-10 20:48:45 +0200

    front page ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,31 @@ import "./App.css";
 function App() {
     return (
         <>
-            <header>
-                <a href="#" className="text-black">
-                    tutorSpace
-                </a>
-                <a href="#">Log in</a>
+            <header className="bg-slate-400 h-20">
+                <div className="max-w-7xl mx-auto px-6 h-full flex justify-between items-center">
+                    <a href="#" className="text-black text-2xl font-bold">
+                        tutorSpace
+                    </a>
+                    <a
+                        href="#"
+                        className="font-bold text-white bg-slate-800 h-10 flex items-center justify-center px-6 rounded-xl transition-colors hover:bg-slate-600"
+                    >
+                        Log in
+                    </a>
+                </div>
             </header>
-            <h1 className="text-3xl font-bold">Hello world!</h1>
+            <div className="h-[calc(100vh-80px)] bg-slate-200">
+                <div className="max-w-7xl mx-auto px-6 h-full flex items-center justify-center flex-col">
+                    <h1 className="font-bold text-5xl mb-8">
+                        Welcome to TutorSpace
+                    </h1>
+                    <p className="sm text-4xl">I am a...</p>
+                    <div className="max-w-xs w-full flex justify-between mt-5">
+                        <a href="#" className="font-bold text-white bg-slate-600 h-10 flex items-center justify-center px-6 rounded-xl transition-colors hover:bg-slate-800">Tutor</a>
+                        <a href="#" className="font-bold text-white bg-slate-600 h-10 flex items-center justify-center px-6 rounded-xl transition-colors hover:bg-slate-800">Student</a>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
